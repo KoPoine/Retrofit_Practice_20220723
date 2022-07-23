7a0e4b69c404799ce040e597ea07ddd3ebe00fc5
김민철 <kmc901206@gmail.com>
2022-07-23 11:49:43 +0900

    Retrofit 관련 클래스 뼈대 작성

---
+++ app/src/main/java/com/neppplus/retrofit_practice_20220723/api/ServerApi.kt
@@ -0,0 +1,30 @@
+package com.neppplus.retrofit_practice_20220723.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ServerApi {
+
+    companion object {
+
+//        서버주소 멤버변수화
+        val baseUrl = "http://54.180.52.26/"
+
+//        retrofit 초기화 진행
+        var retrofit : Retrofit? = null
+
+        fun getRetrofit() : Retrofit {
+
+            if (retrofit == null) {
+                retrofit = Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+
+            return retrofit!!
+        }
+
+    }
+
+}
