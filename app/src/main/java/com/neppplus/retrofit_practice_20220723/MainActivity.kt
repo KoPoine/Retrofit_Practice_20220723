0bd8149a36b37efb9ff46283fb551f4ec6f56d96
김민철 <kmc901206@gmail.com>
2022-07-23 10:20:27 +0900

    데이터 바인딩 세팅 3. kt - xml에 접근하게 작업 진행

--- app/src/main/java/com/neppplus/retrofit_practice_20220723/MainActivity.kt
+++ app/src/main/java/com/neppplus/retrofit_practice_20220723/MainActivity.kt
@@ -2,10 +2,15 @@ package com.neppplus.retrofit_practice_20220723
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.retrofit_practice_20220723.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mBinding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
