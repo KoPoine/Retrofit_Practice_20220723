c759e6de86af2d659568a99679f7a4653461a351
김민철 <kmc901206@gmail.com>
2022-07-23 11:39:41 +0900

    LoginActivity 로그인 기본 로직 작성

--- app/src/main/java/com/neppplus/retrofit_practice_20220723/LoginActivity.kt
+++ app/src/main/java/com/neppplus/retrofit_practice_20220723/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.retrofit_practice_20220723
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -17,7 +18,15 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        mBinding.loginBtn.setOnClickListener {
+            val inputEmail = mBinding.emailEdt.text.toString()
+            val inputPw = mBinding.passwordEdt.text.toString()
 
+//            로그인 API 연결 로직
+            val myIntent = Intent(mContext, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }
     }
 
     override fun setValues() {
