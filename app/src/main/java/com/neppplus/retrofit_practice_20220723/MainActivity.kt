53ad9ba933b9149ab34c30340fd8ea46690d6747
김민철 <kmc901206@gmail.com>
2022-07-23 11:28:03 +0900

    API 라이브러리 설치 - Retrofit

--- app/src/main/java/com/neppplus/retrofit_practice_20220723/MainActivity.kt
+++ app/src/main/java/com/neppplus/retrofit_practice_20220723/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.retrofit_practice_20220723
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -7,12 +8,22 @@ import com.neppplus.retrofit_practice_20220723.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
 
-    lateinit var mBinding : ActivityMainBinding
+    lateinit var mBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
 
+    //    이벤트 동작 로직 작성(setOnClickListener)
+    override fun setupEvents() {
+
+    }
+
+    //    첫 화면 동작시 데이터 연결 (초기화)
+    override fun setValues() {
 
     }
 }
