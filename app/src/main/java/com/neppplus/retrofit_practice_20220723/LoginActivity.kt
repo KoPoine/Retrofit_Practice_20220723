384b007c476c6807eabe59d7a9895b6b12aea705
김민철 <kmc901206@gmail.com>
2022-07-23 11:32:49 +0900

    LoginActivity 데이터 바인딩 초기화

--- app/src/main/java/com/neppplus/retrofit_practice_20220723/LoginActivity.kt
+++ app/src/main/java/com/neppplus/retrofit_practice_20220723/LoginActivity.kt
@@ -2,10 +2,25 @@ package com.neppplus.retrofit_practice_20220723
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.retrofit_practice_20220723.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
+
+    lateinit var mBinding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
