165365cdafae2054ec69aa73c53a3814c3594a57
김민철 <kmc901206@gmail.com>
2022-07-24 10:26:23 +0900

    메인 화면 RecyclerView 표시를 위한 Data Class 생성

--- app/src/main/java/com/neppplus/retrofit_practice_20220723/datas/DataResponse.kt
+++ app/src/main/java/com/neppplus/retrofit_practice_20220723/datas/DataResponse.kt
@@ -2,6 +2,7 @@ package com.neppplus.retrofit_practice_20220723.datas
 
 data class DataResponse (
     val token : String,
-    val user : UserData
+    val user : UserData,
+    val topic : TopicData
         ) {
 }
---
+++ app/src/main/java/com/neppplus/retrofit_practice_20220723/datas/TopicData.kt
@@ -0,0 +1,8 @@
+package com.neppplus.retrofit_practice_20220723.datas
+
+data class TopicData(
+    val id : Int,
+    val title : String,
+    val img_url : String,
+    val reply_count : Int
+)
