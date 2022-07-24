2c72262bd1f9bba6483f9b446f9ef48f9a01faa3
김민철 <kmc901206@gmail.com>
2022-07-24 14:56:59 +0900

    ProfileActivity xml 작성

--- app/src/main/java/com/neppplus/retrofit_practice_20220723/ProfileActivity.kt
+++ app/src/main/java/com/neppplus/retrofit_practice_20220723/ProfileActivity.kt
@@ -2,8 +2,12 @@ package com.neppplus.retrofit_practice_20220723
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.retrofit_practice_20220723.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    lateinit var mBinding : ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
