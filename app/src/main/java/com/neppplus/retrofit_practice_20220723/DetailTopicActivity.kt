32a9e65e197af328d9890763cadaf51eb422f9bc
김민철 <kmc901206@gmail.com>
2022-07-24 13:39:58 +0900

    서버에서 가져온 데이터를 UI에 반영

--- app/src/main/java/com/neppplus/retrofit_practice_20220723/DetailTopicActivity.kt
+++ app/src/main/java/com/neppplus/retrofit_practice_20220723/DetailTopicActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.retrofit_practice_20220723
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.neppplus.retrofit_practice_20220723.databinding.ActivityDetailTopicBinding
 import com.neppplus.retrofit_practice_20220723.datas.BasicResponse
 import com.neppplus.retrofit_practice_20220723.datas.TopicData
@@ -32,13 +33,19 @@ class DetailTopicActivity : BaseActivity() {
 
     override fun setValues() {
 //가져온 데이터 UI 처리 등등
+        getDetailTopicDataFromServer()
+
+        setTopicDataToUi()
+    }
+
+    fun getDetailTopicDataFromServer() {
         val token = ContextUtil.getLoginToken(mContext)
         apiList.getRequestDetailTopic(token, mTopicData.id).enqueue(object : Callback<BasicResponse>{
             override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
                 if (response.isSuccessful) {
                     val br = response.body()!!
 
-                    Log.d("상세 토픽 조회 결과", br.data.topic.toString())
+                    mTopicData = br.data.topic
                 }
                 else {
                     val errorBodyStr = response.errorBody()!!.string()
@@ -53,6 +60,43 @@ class DetailTopicActivity : BaseActivity() {
 
             }
         })
+    }
 
+//    가져온 토픽 데이터를 UI에 반영하는 코드
+
+    fun setTopicDataToUi() {
+        mBinding.titleTxt.text = mTopicData.title
+        Glide.with(mContext)
+            .load(mTopicData.img_url)
+            .into(mBinding.backgroundImg)
+
+        mBinding.side1TitleTxt.text = mTopicData.sides[0].title
+        mBinding.side2TitleTxt.text = mTopicData.sides[1].title
+
+        mBinding.side1VoteCountTxt.text = "${mTopicData.sides[0].vote_count}표"
+        mBinding.side2VoteCountTxt.text = "${mTopicData.sides[1].vote_count}표"
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
